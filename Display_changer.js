2def6918bea59f83b96cdfbe444798780adf56c6
13frogs <133548115+13frogs@users.noreply.github.com>
2024-12-19 19:53:12 -0500

    Update Display_changer.js functions

--- Display_changer.js
+++ Display_changer.js
@@ -1,7 +1,37 @@
-function NextVideo() {
-  //changes to next random video, description, and data
+const adAstra = {
+  summary: "Thirty years ago, Clifford McBride led a voyage into deep space, but the ship and crew were never heard from again. Now his son -- a fearless astronaut -- must embark on a daring mission to Neptune to uncover the truth about his missing father and a mysterious power surge that threatens the stability of the universe.",
+  year: "2019",
+  genre: "2h 4m",
+  trailer: "https://www.youtube.com/watch?v=BsCNKuB93BA",
+  hulu: true,
+  disney: true,
+  amazon: false,
+  netflix: false
+};
+
+const movies = new Array(adAstra); 
+let video = "";
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
 }
+
+let hasService = false;
 
 function CheckServices() {
   //checks if user has one of the designated services
 }
+
+function NextVideo() {
+  let number = getRndInteger(0, movies.length);
+  while (hasService == false) {
+    if (checkServices(number) == false) {
+       let number = getRndInteger(0, movies.length);
+    } else {
+      let hasService = true;
+    }
+  }
+  let video = movies[number].trailer;
+  return video;
+}
+
